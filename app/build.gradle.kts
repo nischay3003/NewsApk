62aa327c96e3025802a5437b1035547ccbbb2479
nischay <nischay728@gmail.com>
2024-06-09 18:55:11 +0530

    Intial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.newsapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.newsapp"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core);
+
+    implementation ("com.amitshekhar.android:android-networking:1.0.2")
+    implementation ("com.amitshekhar.android:jackson-android-networking:1.0.2'");
+}
